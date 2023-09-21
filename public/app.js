9394d9a3d003df6aa7c718540af04705ee0167a2
Iamneo <techadmin@iamneo.ai>
2023-09-21 10:23:53 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -7,21 +7,25 @@ if(amount<1000){
 }
 else if(amount>1000 && amount<5000){
     let discount = 5
-    document.getElementById("discountedAmount").innerHTML = "Discounted Amount: $"+ amount-amount*(discount/100);
+    let x = amount-amount*(discount/100)
+    document.getElementById("discountedAmount").innerHTML = "Discounted Amount: $"+ x;
+    console.log(x)
 
 }
 else if(amount>5001 && amount<7500){
     let discount = 10
-    document.getElementById("discountedAmount").innerHTML = "Discounted Amount: $"+ amount-amount*(discount/100);
+    var x = amount-amount*(discount/100)
+    document.getElementById("discountedAmount").innerHTML = "Discounted Amount: $"+ x;
     document.getElementById("discountedAmount").style.color = "green"
     
 
 }
 else if(amount>7500){
     let discount = 15
-    document.getElementById("discountedAmount").innerHTML = "Discounted Amount: $"+ amount-amount*(discount/100);
+    var x = amount-amount*(discount/100)
+    document.getElementById("discountedAmount").innerHTML = "Discounted Amount: $"+ x;
     document.getElementById("discountedAmount").style.color = "green"
-    document.getElementById("discountedAmount").style = "bold"
+    document.getElementById("discountedAmount").style.fontWeight = "bold"
 
 }
 
