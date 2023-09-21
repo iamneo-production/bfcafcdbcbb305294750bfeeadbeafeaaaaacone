b6bda48cee11e05acca850c354017bc29f209219
Iamneo <techadmin@iamneo.ai>
2023-09-21 10:03:09 +0000

    Backup Commit

---
+++ public/app.js
@@ -0,0 +1,13 @@
+function calc(){
+const amount = document.getElementById("amountInput").value;
+
+if(amount<1000){
+    document.getElementById("discountedAmount").value = "Discounted Amount: "+ amount;
+  
+}
+else if(amount>1000 && amount<5000){
+    
+
+}
+
+}
